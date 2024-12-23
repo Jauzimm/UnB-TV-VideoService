e1ddfa19b33229babc5c1f162642f93bca1216a8
Jauzimm <joaovitorso071@gmail.com>
2024-12-23 10:15:52 -0300

    Resolvendo o caminho a getvideorecomendation

--- src/recommendation_model/get_video_recommendation.py
+++ src/recommendation_model/get_video_recommendation.py
@@ -1,24 +1,15 @@
 import pandas as pd
 import pickle
-import os
 
 # Obtém uma lista de vídeos recomendados a partir de um ID
 def get_recommendations(id):
 
     cosine_path = 'src/recommendation_model/cosine_similarity.pkl'
-    # Teste para garantir que o arquivo está acessível
-    if not os.path.exists(cosine_path):
-        raise FileNotFoundError(f"Arquivo não encontrado: {cosine_path}")
-    
-    # Imprime a estrutura completa do diretório 'src' para verificar a organização
-    print("Estrutura do diretório src:", os.listdir('/app/src'))
-    
-    print("Conteúdo do diretório recommendation_model:", os.listdir('/app/src/recommendation_model'))
 
     with open(cosine_path, 'rb') as f:
         cosine_sim = pickle.load(f)
 
-    df = pd.read_csv('/app/src/recommendation_model/df_videos.csv')
+    df = pd.read_csv('src/recommendation_model/df_videos.csv')
 
     indices = pd.Series(df.index, index=df['ID']).drop_duplicates()
 
